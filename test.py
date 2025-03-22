966c4210b7ca15c073dd9516fcaf04d03cc9d97a
umcann123 <likey4978@yahoo.com.tw>
2025-03-23 00:26:14 +0800

    Stanley

--- test.py
+++ test.py
@@ -2,3 +2,4 @@ for i in range(3):
     food = ['apple', 'banana', 'orange']
     price = [30, 25, 35]
     print(f"Food: {food[i]}, Price: ${price[i]}")
+print("I think becky should pay for the food for stanley")
