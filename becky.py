9a0c1550cdab36b6769445360d7de2c8f72762ae
becky0919 <becky87919@gmail.com>
2025-03-23 12:38:29 +0800

    becky update 0323

--- becky.py
+++ becky.py
@@ -1,1 +1,6 @@
-print("I like pupu and furfur")
+print("Ｗhich one do you like the most? Becky,Pupu or Furfur?")
+user_input = input("請輸入答案：")
+if user_input == "Becky":
+    print("Correct!")
+else:
+    print("Wrong answer!")
